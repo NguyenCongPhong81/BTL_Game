9070fa1a872a9654bed1e6c327925e9e3e4d6337
dangdinhphuong1 <2051063780@e.tlu.edu.vn>
2024-01-11 22:38:20 +0700

    update code
    update code logic player

---
+++ Assets/Scripts/MathRunWoodItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MathRun
+{
+    public class MathRunWoodItem : MonoBehaviour
+    {
+        public int Index { get; set; } = 0;
+
+    }
+}
